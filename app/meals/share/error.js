69cac485554eeba5496afa33040aeea065062f72
onyedika-anagha <paschalanagha@gmail.com>
2024-04-11 09:43:31 +0100

    updated error handling with useFormState

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error({ error }) {
+  return (
+    <main className="error">
+      <h1>An error occured!</h1>
+      <p>Failed to create meal, Please try again.</p>
+    </main>
+  );
+}
